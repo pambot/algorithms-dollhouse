fixed normalization

dollhouse/toolshed/statistics.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 
-def normalize(v):
-    return (v - np.max(v)) / (np.max(v) - np.min(v))
+def normalize(v, by_column=False):
+    axis = 0 if by_column else None
+    return (v - np.mean(v, axis=axis)) / (np.max(v, axis=axis) - np.min(v, axis=axis))
 
 
 def gaussian(x, mean, standard_deviation):