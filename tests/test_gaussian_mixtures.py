0e146239935dcb38a0b078cc1ddc167decb0d006
pambot <thefourtheye@gmail.com>
2021-08-08 13:35:37 -0400

    black reformatting

--- tests/test_gaussian_mixtures.py
+++ tests/test_gaussian_mixtures.py
@@ -4,18 +4,11 @@ from dollhouse.gaussian_mixtures import GaussianMixture1D
 
 np.random.seed(0)
 
-def test_gaussian_mixture_1d():
-    X = np.concatenate([
-        np.random.normal(10, 1, 100),
-        np.random.normal(1, 2, 100)
-    ])
 
-    y_expected = np.concatenate([
-        np.zeros(100),
-        np.ones(100)
-    ])
+def test_gaussian_mixture_1d():
+    X = np.concatenate([np.random.normal(10, 1, 100), np.random.normal(1, 2, 100)])
+
+    y_expected = np.concatenate([np.zeros(100), np.ones(100)])
 
     y_predicted = GaussianMixture1D(2).fit(X)
     assert_array_equal(y_predicted, y_expected)
-
-
--- tests/test_linear_regression.py
+++ tests/test_linear_regression.py
@@ -6,8 +6,11 @@ from dollhouse.linear_regression import LinearRegression
 
 np.random.seed(0)
 
+
 def test_linear_regression():
-    X, y, coefficients = make_regression(n_samples=1000, n_features=4, bias=0, noise=1, random_state=0, coef=True)
+    X, y, coefficients = make_regression(
+        n_samples=1000, n_features=4, bias=0, noise=1, random_state=0, coef=True
+    )
     X_train, y_train, X_test, y_test = train_test_split(X, y, 990)
 
     lr = LinearRegression()
@@ -16,6 +19,3 @@ def test_linear_regression():
 
     assert_allclose(y_predicted, y_test, rtol=0.1)
     assert_allclose(lr.coefficients, coefficients, rtol=0.01)
-    
-
-    
